06b473aa234bdd7e96e698b483c95b35bcce67e4
Sarvesh Mhadgut <sarveshmhadgut@icloud.com>
2025-11-20 12:46:25 +0530

    initialize project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An end to end MLOps pipeline for Movie Reviews Sentiment Analysis.',
+    author='Sarvesh Mhadgut',
+    license='',
+)
